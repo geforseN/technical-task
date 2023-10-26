04f2ec08617c8b1dbe1d3aa8047d62f8a5f457b6
geforseN <semen.neb@gmail.com>
2023-10-26 22:03:10 +0700

    SelectedCard: change
    fix templates
    make required buttons
    change styles

--- components/unload/SelectedCard.vue
+++ components/unload/SelectedCard.vue
@@ -3,24 +3,27 @@
     <div class="header base-border-radius">
       <span class="id text-bold">{{ cardResponse.data.id }}</span>
       <span class="event" v-html="cardResponse.data.event" />
-      <button class="close-selected-card-btn" @click="emit('close')"><div class="fas fa-xmark" /></button>
+      <button class="top-btn-close" @click="emit('close')"><div class="fas fa-xmark" /></button>
     </div>
-    <UnloadBlock data-color="light-purple">
-      <div class="info base-border-radius">
-        <div class="fas fa-lightbulb-on" />
-        Если после клика на ссылку загрузка не пошла, проверьте не блокирует ли браузер скачивание архива.
-      </div>
-      <div>
-        <h2 class="download-link-label text-bold">Ссылка для скачивания архива Выгрузки (.zip):</h2>
-        <div class="download-link-container">
-          <a class="link" :href="cardResponse.data.downloadLink">
-            {{ cardResponse.data.downloadLink }}
-          </a>
-          <button class="copy-download-link-btn" @click="handleClipboardCopy(cardResponse.data.downloadLink)">
-            <span class="span-link">Скопировать ссылку</span>
-          </button>
+    <UnloadBlock>
+      <template #above>
+        <div class="info base-border-radius">
+          <div class="fas fa-lightbulb-on" />
+          Если после клика на ссылку загрузка не пошла, проверьте, не блокирует ли браузер скачивание архива.
         </div>
+      </template>
+      <h2 class="heading text-bold">Ссылка для скачивания архива Выгрузки (.zip):</h2>
+      <div class="link-container">
+        <a class="link" :href="cardResponse.data.downloadLink">
+          {{ cardResponse.data.downloadLink }}
+        </a>
+        <button class="btn-copy-link" @click="handleClipboardCopy(cardResponse.data.downloadLink)">
+          <span class="span-link">Скопировать ссылку</span>
+        </button>
       </div>
+      <template #below>
+        <button class="button bottom-btn-close" @click="emit('close')">Закрыть</button>
+      </template>
     </UnloadBlock>
   </div>
 </template>
@@ -40,61 +43,78 @@ function handleClipboardCopy(value: string) {
 </script>
 
 <style lang="scss" scoped>
-.download-link-label {
-  font-size: 16px;
-}
-
-.selected-card {
-  position: sticky;
-  top: 20px;
-}
-
-.fa-lightbulb-on {
-  color: var(--color_blue);
-}
-.id {
-  display: flex;
-  padding: 10px;
-  align-items: center;
-  color: white;
-  background-color: var(--color_primary);
-}
-
-.download-link-container {
-  display: flex;
-  gap: 10px;
-}
-
 .header {
   display: flex;
   height: 40px;
+
+  .id {
+    display: flex;
+    padding: 10px;
+    align-items: center;
+    color: white;
+    background-color: var(--color_primary);
+  }
+
+  .event {
+    display: flex;
+    align-items: center;
+    background-color: #f5f5f9;
+    padding: 10px;
+    flex-grow: 1;
+  }
+
+  .top-btn-close {
+    display: none;
+
+    @include start-at("xslg") {
+      display: block;
+      background-color: var(--color_danger);
+      border: 0;
+      padding: 10px;
+      color: white;
+    }
+  }
 }
+
+.link-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0 10px;
+
+  .btn-copy-link {
+    background-color: white;
+    border: 0;
+    padding: 0;
+  }
+}
+
 .info {
   padding: 10px;
   background-color: var(--color_very_light_blue);
+
+  .fa-lightbulb-on {
+    color: var(--color_blue);
+  }
 }
 
-.event {
-  display: flex;
-  align-items: center;
-  background-color: #f5f5f9;
-  padding: 10px;
-  flex-grow: 1;
+.heading {
+  font-size: 16px;
 }
 
-.close-selected-card-btn {
+.info ~ .heading {
+  margin-top: 5px;
+}
+
+.button.bottom-btn-close {
+  display: block;
   background-color: var(--color_danger);
-  border: 0;
-  padding: 10px;
+  padding: 10px 30px;
   color: white;
-}
+  margin-left: auto;
+  margin-top: 20px;
 
-.block {
-  gap: 10px;
-}
-
-.copy-download-link-btn {
-  background-color: white;
-  border: 0;
+  @include start-at("xslg") {
+    display: none;
+  }
 }
 </style>
