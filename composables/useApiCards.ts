fb5a788c540d0cf1057d63aa502f6b4765f91617
geforseN <semen.neb@gmail.com>
2023-10-26 19:46:17 +0700

    change useApiCards composable

--- composables/useApiCards.ts
+++ composables/useApiCards.ts
@@ -1,18 +1,36 @@
-export type Card = {
+export type ApiCard = {
   date: string;
   event: string;
-  id: string;
+  id: `${number}`;
   size: string;
   status: string;
   status_text: string;
   task_date: string;
 };
 
-export const useApiCards = (data: Ref<Card>) => {
-  return useAPIFetch("e.scripts", {
+export type Card = Omit<ApiCard, "status_text" | "task_date"> & {
+  statusText: ApiCard["status_text"];
+  taskDate: ApiCard["task_date"];
+};
+
+export const useApiCards = () => {
+  return useAPIFetch<Card[]>("e.scripts", {
     params: { page: "pages:unload", event: "get" },
     onResponse: context => {
-      data.value = JSON.parse(context.response._data).response.data;
+      const cards: ApiCard[] = JSON.parse(context.response._data).response.data;
+      context.response._data = cards.map(toUppercaseCard);
     }
   });
 };
+
+export function toUppercaseCard(apiCard: ApiCard): Card {
+  return {
+    date: apiCard.date,
+    event: apiCard.event,
+    id: apiCard.id,
+    size: apiCard.size,
+    status: apiCard.status,
+    statusText: apiCard.status_text,
+    taskDate: apiCard.task_date
+  };
+}
