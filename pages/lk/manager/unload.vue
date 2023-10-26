6608d7121e2e216d4a27c6f21294f347f7e14864
geforseN <semen.neb@gmail.com>
2023-10-26 22:04:58 +0700

    unload page: change
    make required styles
    update template layout

--- pages/lk/manager/unload.vue
+++ pages/lk/manager/unload.vue
@@ -1,15 +1,11 @@
 <template>
   <main class="unload-container">
-    <div class="left">
-      <UnloadInfoBlock />
-      <template v-if="cardsResponse.data">
-        <UnloadCard v-for="card of cardsResponse.data" v-bind="card" :key="card.id" @click="selectedCard = card" />
-      </template>
+    <UnloadInfoBlock class="info" />
+    <div v-if="cardsResponse.data" class="cards">
+      <UnloadCard v-for="card of cardsResponse.data" v-bind="card" :key="card.id" @click="selectedCard = card" />
     </div>
-    <div class="right">
-      <UnloadHelpBlock v-if="!selectedCard" />
-      <UnloadSelectedCard v-else :card-response="cardResponse" @close="selectedCard = null" />
-    </div>
+    <UnloadHelpBlock v-if="!selectedCard" class="right" />
+    <UnloadSelectedCard v-else class="right" :card-response="cardResponse" @close="selectedCard = null" />
   </main>
 </template>
 
@@ -31,22 +27,40 @@ const cardsResponse = ref(await useApiCards());
 
 <style lang="scss" scoped>
 .unload-container {
-  display: flex;
-  position: sticky;
-  top: 10px;
-  left: 0;
-  gap: 15px;
   font-size: 14px;
   line-height: 1.2;
+
+  display: grid;
+  gap: 15px;
+
+  grid-template-areas:
+    "info"
+    "right"
+    "cards";
+
+  @include start-at("xslg") {
+    grid-template-columns: 35% 1fr;
+    grid-template-areas:
+      "info right"
+      "cards right";
+  }
 }
 
-.left {
-  width: 35%;
+.info {
+  grid-area: info;
+}
+
+.cards {
+  grid-area: cards;
   display: grid;
   gap: 10px;
 }
 
 .right {
-  width: 65%;
+  grid-area: right;
+  display: block;
+  position: sticky;
+  top: 10px;
+  height: max-content;
 }
 </style>
