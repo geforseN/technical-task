169f4486ad5345356493090eea23f53f8fab7178
geforseN <semen.neb@gmail.com>
2023-10-26 22:00:01 +0700

    Block.vue: add optional slots

--- components/unload/Block.vue
+++ components/unload/Block.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="block">
+    <slot name="above" />
+    <slot name="heading" />
     <slot />
+    <slot name="below" />
   </div>
 </template>
 
 <script setup lang="ts">
 defineSlots<{
+  above?: () => any;
+  heading?: () => any;
   default: () => any;
+  below?: () => any;
 }>();
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .block {
   display: grid;
   padding: 15px;
